353a4aa022e724e36beb3318adb870e61761cdd0
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-05-18 09:55:16 +0000

    Add main file

---
+++ main.c
@@ -0,0 +1,56 @@
+#include "shell.h"
+
+/**
+ * main - entry point for program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+int main(int argc, __attribute__((unused))char **argv)
+{
+	char *input = NULL;
+	size_t input_size = 0;
+	char *args[MAX_ARGS];
+	int interactive;
+	int fd;
+
+	interactive = isatty(STDIN_FILENO);
+	signal(SIGINT, sigint_handler);
+
+	while (1)
+	{
+		if (interactive)
+		{
+			write(STDOUT_FILENO, "$ ", 2);
+		}
+
+		if (_getline(&input, &input_size, stdin) == -1)
+			break;
+
+		argc = parse_command(input, args);
+
+		if (argc > 0)
+		{
+			if (_strcmp(args[0], "exit") == 0)
+				handle_exit(args);
+else if (_strcmp(args[0], "setenv") == 0)
+	{
+		fd = _setenv(args[1], args[2]);
+		if (fd == -1)
+		{
+			perror("setenv");
+			exit(-1);
+		}
+		/* Set environment variable */
+	}
+
+			else if (is_builtin_command(args[0]))
+				run_builtin_command(args);
+			else
+				run_external_command(args);
+		}
+	}
+	free(input);
+
+	return (EXIT_SUCCESS);
+}
