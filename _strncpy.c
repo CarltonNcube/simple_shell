47dcf4fe0771818a2d4f23a4543ffccf68e12204
CarltonNcube <carltonncube@outlook.com>
2023-05-21 05:31:08 -0700

    copy a string

---
+++ _strncpy.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * _strncpy - copy a string
+ * @destination: destination of copy
+ * @source: source of copy
+ * @num: number of characters to be copied
+ * Return: pointer to destination string
+ */
+
+char *_strncpy(char *destination, const char *source, size_t num)
+{
+	char *dest = destination;
+	size_t i;
+
+	for (i = 0; i < num && *source != '\0'; i++)
+		*dest++ = *source++;
+		*dest++ = '\0';
+
+	for (; i < num; i++)
+	return (destination);
+}
