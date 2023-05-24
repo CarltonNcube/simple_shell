9c508bed3fa8b3147e8b5fab031fd7e86ef1bae6
CarltonNcube <carltonncube@outlook.com>
2023-05-24 13:30:53 -0700

    change directory and update PWD and OLPWD

--- change_dir.c
+++ change_dir.c
@@ -1,18 +1,21 @@
-#include "shell.h"/**
+#include "shell.h"
+
+/**
  * change_directory - change directory and update PWD and OLPWD
  * @path: specified directory
  * Return: void
  */
+
 void change_directory(const char *path)
 {
-	char* old_pwd = _getenv("PWD"); /* Current working directory */
+	char *old_pwd = _getenv("PWD");		/* Current working directory */
 	char *new_pwd = NULL;
 
 	if (old_pwd)
-		_setenv("OLDPWD", old_pwd); /* Update PWD with current */
-	
+		_setenv("OLDPWD", old_pwd);	/* Update PWD with current */
+
 	if (chdir(path) == 0)
-        {
+	{
 		new_pwd = get_current_directory();
 		if (new_pwd)
 			_setenv("PWD", new_pwd);
