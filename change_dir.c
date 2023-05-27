93a592078607edbecf5e995bdf62f2250af2c3cb
CarltonNcube <carltonncube@outlook.com>
2023-05-27 11:32:15 -0700

    change directory and update PWD and OLPWD

--- change_dir.c
+++ change_dir.c
@@ -5,27 +5,21 @@
  * @path: specified directory
  * Return: void
  */
-
 void change_directory(const char *path)
 {
-	char *old_pwd = _getenv("PWD");/* Current working directory */
+	char* old_pwd = _getenv("PWD"); /* Current working directory */
 	char *new_pwd = NULL;
 
 	if (old_pwd)
-		_setenv("OLDPWD", old_pwd);/* Update PWD with current */
-
+		_setenv("OLDPWD", old_pwd); /* Update PWD with current */
 	if (chdir(path) == 0)
 	{
-		char cwd[PATH_MAX];
-
-		if (getcwd(cwd, sizeof(cwd)) != NULL)
-		{
-			new_pwd = get_current_directory();
+		new_pwd = get_current_directory();
+		if (new_pwd)
 			_setenv("PWD", new_pwd);
-		}
 		else
-			perror("cd");/* Print error message */
+			perror("cd"); /* Print error message */
 	}
 	else
-		perror("cd");/* Print error message */
+		perror("cd"); /* Print error message */
 }
