db2c50da9fd7616f140f8cb4dc3b1ee8814b970b
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-05-27 20:20:41 +0000

    [cd]: Handle memory leak

--- get_current_dir.c
+++ get_current_dir.c
@@ -6,18 +6,27 @@
  */
 char *get_current_directory()
 {
-	char *cwd = (char *) malloc(256);
+	char *cwd = NULL;
+	size_t size = 256; /* Initial buffer size */
 
-	if (cwd != NULL)
+	while (1)
 	{
-		if (getcwd(cwd, PATH_MAX) != NULL)
-			return (cwd);
-		else
+		cwd = (char *)malloc(size);
+		if (cwd == NULL)
 		{
-			perror("cwd");
-			free(cwd);
+			perror("malloc");
+			return (NULL);
 		}
-	}
+		if (getcwd(cwd, size) != NULL)
+			return (cwd);
 
+		free(cwd);
+		if(errno != ERANGE)
+		{
+			perror("getcwd");
+			return (NULL);
+		}
+		size *= 2; /* Double the buffer size for next attempt */
+	}
 	return (NULL);
 }
